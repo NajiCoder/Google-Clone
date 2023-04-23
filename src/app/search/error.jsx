6d645d4b783fabb5d63a0adf961f2f35a54c714c
Naji <lemiine987@gmail.com>
2023-04-23 12:26:47 +0300

    Handle possible errors

---
+++ src/app/search/error.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { useEffect } from "react";
+
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.log("Error: ", error);
+  }, [error]);
+
+  return (
+    <div className="flex flex-col justify-center items-center pt-10">
+      <h1 className="text-3xl mb-4">
+        Something went wrong while fetching data
+      </h1>
+      <button className="text-blue-400" onClick={() => reset()}>
+        Try again
+      </button>
+    </div>
+  );
+}
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export default async function WebSerch({ searchParams }) {
@@ -5,9 +6,31 @@ export default async function WebSerch({ searchParams }) {
     `https://www.googleapis.com/customsearch/v1?key=${process.env.SEARCH_API}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
 
+  if (!res.ok) {
+    throw new Error(
+      "Something went wrong while fetching data from Google Search API"
+    );
+  }
+
   const data = await res.json();
 
   const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">
+          No results found for {searchParams.searchTerm}
+        </h1>
+        <p className="text-lg">
+          Try searching someting else or go back to the homepage {"    "}
+          <Link href={"/"} className="text-blue-500 ">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
 
   return (
     <>
