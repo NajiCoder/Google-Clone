eac64a6a8f426080436ae19c2b185c39307f51b7
Naji <lemiine987@gmail.com>
2023-04-23 11:56:14 +0300

    Show search from google search API

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function WebSerch() {
-  return <div>WebSerch</div>;
+export default async function WebSerch({ searchParams }) {
+  const res = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.SEARCH_API}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+
+  const data = await res.json();
+
+  const results = data.items;
+
+  return (
+    <>
+      {results &&
+        results.map((result, index) => <h1 key={index}>{result.title}</h1>)}
+    </>
+  );
 }
